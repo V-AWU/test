34e2ad32cc67a6a7516daee26d90b628d497a184
V-AWU <vawu560@gmail.com>
2025-08-10 21:32:11 +0100

    Update openWindow.cpp

--- openWindow.cpp
+++ openWindow.cpp
@@ -2,44 +2,53 @@
 
 // Window procedure
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    if (uMsg == WM_DESTROY) {
-        PostQuitMessage(0);
-        return 0;
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
     }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return DefWindowProcA(hwnd, uMsg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
     const char CLASS_NAME[] = "BorderlessWindowClass";
 
-    // Register window class
-    WNDCLASS wc = {};
+    // Register window class (ANSI version)
+    WNDCLASSA wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); // white background
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // white background
 
-    RegisterClass(&wc);
+    if (!RegisterClassA(&wc)) {
+        MessageBoxA(NULL, "Failed to register window class", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
 
     // Create borderless window
-    HWND hwnd = CreateWindowEx(
-        0,                     // Extended style
-        CLASS_NAME,            // Class name
-        "",                    // No title
-        WS_POPUP,              // Borderless window
+    HWND hwnd = CreateWindowExA(
+        0,                    // Extended style
+        CLASS_NAME,           // Class name
+        "",                   // No title
+        WS_POPUP,             // Borderless window style
         CW_USEDEFAULT, CW_USEDEFAULT,
-        800, 600,              // Size
+        800, 600,             // Size
         NULL, NULL, hInstance, NULL
     );
+
+    if (!hwnd) {
+        MessageBoxA(NULL, "Failed to create window", "Error", MB_OK | MB_ICONERROR);
+        return 0;
+    }
 
     ShowWindow(hwnd, SW_SHOW);
 
     // Message loop
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    while (GetMessageA(&msg, NULL, 0, 0) > 0) {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        DispatchMessageA(&msg);
     }
 
-    return 0;
+    return (int)msg.wParam;
 }
